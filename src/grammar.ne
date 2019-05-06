c0c8a571f6f35deefdf8217129dbd09210891876
Christopher Hlubek <hlubek@networkteam.com>
2019-05-06 11:31:57 +0200

    Add tests, prepare release

--- src/grammar.ne
+++ src/grammar.ne
@@ -1,21 +1,61 @@
-main -> disjunction {% d => d[0] %}
+expression -> conditional {% d => d[0] %}
 
-parens -> "(" _ disjunction _ ")" {% d => '(' + d[2] + ')' %}
+# TODO Check if associativness of ternary is correct
+conditional -> disjunction _ "?" _ expression _ ":" _ expression  {% d => d[0] + "?" + d[4] + ":" + d[8] %}
+             | disjunction
+
+parens -> "(" _ expression _ ")" {% d => '(' + d[2] + ')' %}
         | atom {% d => d[0] %}
 
 disjunction -> disjunction _ "||" _ conjunction {% d => d[0] + '||' + d[4] %}
              | conjunction {% d => d[0] %}
 
-conjunction -> conjunction _ "&&" _ negation {% d => d[0] + '&&' + d[4] %}
-             | negation {% d => d[0] %}
+conjunction -> conjunction _ "&&" _ comparison {% d => d[0] + '&&' + d[4] %}
+             | comparison {% d => d[0] %}
 
-negation -> "!" _ parens {% d => '!' + d[2] %}
+comparison -> comparison _ COMPARISON_OP _ sumcalc {% d => d[0] + d[2] + d[4] %}
+            | sumcalc {% d => d[0] %}
+
+sumcalc -> sumcalc _ SUM_OP _ prodcalc {% d => d[0] + d[2] + d[4] %}
+         | prodcalc {% d => d[0] %}
+
+prodcalc -> prodcalc _ PROD_OP _ negation {% d => d[0] + d[2] + d[4] %}
+         | negation {% d => d[0] %}
+
+negation -> "!" _ negation {% d => '!' + d[2] %}
           | parens {% d => d[0] %}
 
 atom -> IDENTIFIER {% d => d[0] %}
       | NUMBER {% d => d[0] %}
+      | dqstring  {% d => d[0] %}
+      | sqstring  {% d => d[0] %}
 
 IDENTIFIER -> [a-zA-Z_] [a-zA-Z_0-9]:* {% d => 'ctx.' + d[0] + d[1].join('') %}
-NUMBER -> [0-9]:+ {% d => parseInt(d[0].join(''), 10) %}
+NUMBER -> "-":? [0-9]:+ {% d => parseInt((d[0] || '') + d[1].join(''), 10) %}
+
+COMPARISON_OP -> "==" {% d => d[0] %}
+               | "!=" {% d => d[0] %}
+               | "<=" {% d => d[0] %}
+               | ">=" {% d => d[0] %}
+               | "<" {% d => d[0] %}
+               | ">" {% d => d[0] %}
+
+SUM_OP -> "+" {% d => d[0] %}
+        | "-" {% d => d[0] %}
+
+PROD_OP -> "*" {% d => d[0] %}
+         | "/" {% d => d[0] %}
+         | "%" {% d => d[0] %}
+
+dqstring -> "\"" dstrchar:* "\"" {% d => '"' + d[1].join('') + '"' %}
+dstrchar -> [^\\"\n] {% id %}
+          | "\\" strescape {% d => d.join('') %}
+
+sqstring -> "'" sstrchar:* "'" {% d => "'" + d[1].join('') + "'" %}
+sstrchar -> [^\\'\n] {% id %}
+          | "\\" strescape {% d => d.join('') %}
+
+strescape -> ["'\\/bfnrt] {% id %}
+    | "u" [a-fA-F0-9] [a-fA-F0-9] [a-fA-F0-9] [a-fA-F0-9] {% d => d.join('') %}
 
 _ -> [\s]:* {% d => null %}
---
+++ test/test.compile.js
@@ -0,0 +1,174 @@
+import compile from '../src/compile.js';
+import assert from 'assert';
+
+describe('compile', function() {
+  describe('number literals', function() {
+    describe('integer without sign', function() {
+      it('should parse and evaluate to literal value', function() {
+        const fn = compile('42');
+        const ctx = {};
+        assert.strictEqual(fn(ctx), 42);
+      });
+    });
+
+    describe('negative integer', function() {
+      it('should parse and evaluate to literal value', function() {
+        const fn = compile('-1');
+        const ctx = {};
+        assert.strictEqual(fn(ctx), -1);
+      });
+    });
+  });
+
+  describe('string literals', function() {
+    describe('double quoted', function() {
+      it('should parse and evaluate to literal value', function() {
+        const fn = compile(`"foo"`);
+        const ctx = {};
+        assert.strictEqual(fn(ctx), `foo`);
+      });
+
+      describe('with escapes', function() {
+        it('should parse and evaluate to literal value with escape sequences', function() {
+          const fn = compile(`"foo\\"next'thingy'"`);
+          const ctx = {};
+          assert.strictEqual(fn(ctx), `foo"next'thingy'`);
+        });
+
+        it('should parse and evaluate newlines', function() {
+          const fn = compile(`"multi\\nline\\ntext"`);
+          const ctx = {};
+          assert.strictEqual(fn(ctx), `multi\nline\ntext`);
+        });
+      })
+    });
+
+    describe('single quoted', function() {
+      it('should parse and evaluate to literal value', function() {
+        const fn = compile(`'foo'`);
+        const ctx = {};
+        assert.strictEqual(fn(ctx), `foo`);
+      });
+
+      describe('with escapes', function() {
+        it('should parse and evaluate to literal value with escape sequences', function() {
+          const fn = compile(`'foo\\'next"thingy"'`);
+          const ctx = {};
+          assert.strictEqual(fn(ctx), `foo'next"thingy"`);
+        });
+
+        it('should parse and evaluate newlines', function() {
+          const fn = compile(`'multi\\nline\\ntext'`);
+          const ctx = {};
+          assert.strictEqual(fn(ctx), `multi\nline\ntext`);
+        });
+      })
+    });
+  });
+
+  describe('variables', function() {
+    it('should parse and evaluate to variable in context', function() {
+      const fn = compile('myVar');
+      const ctx = {myVar: 'foo'};
+      assert.strictEqual(fn(ctx), ctx.myVar);
+    });
+  });
+
+  describe('boolean operators', function() {
+    describe('conjunction', function() {
+      it('should parse and short circuit evaluate to operand', function() {
+        const fn = compile('varA &&varB');
+        assert.strictEqual(fn({varA: 'x', varB: 0}), 0);
+        assert.strictEqual(fn({varA: 'x', varB: 1}), 1);
+        assert.strictEqual(fn({varA: 0, varB: 'foo'}), 0);
+      });
+
+      it('should support multiple conjunctios', function() {
+        const fn = compile('varA &&varB&& varC');
+        assert.strictEqual(fn({varA: 'x', varB: 0, varC: 1}), 0);
+        assert.strictEqual(fn({varA: 'x', varB: 42, varC: false}), false);
+        assert.strictEqual(fn({varA: 'x', varB: 42, varC: true}), true);
+      });
+    });
+
+    describe('disjunction', function() {
+      it('should parse and short circuit evaluate to operand', function() {
+        const fn = compile('varA ||varB');
+        assert.strictEqual(fn({varA: 'x', varB: 0}), 'x');
+        assert.strictEqual(fn({varA: 'x', varB: 1}), 'x');
+        assert.strictEqual(fn({varA: 0, varB: 'foo'}), 'foo');
+      });
+
+      it('should support multiple conjunctios', function() {
+        const fn = compile('varA ||varB|| varC');
+        assert.strictEqual(fn({varA: 'x', varB: 0, varC: 1}), 'x');
+        assert.strictEqual(fn({varA: 42, varB: 42, varC: false}), 42);
+        assert.strictEqual(fn({varA: false, varB: 42, varC: true}), 42);
+        assert.strictEqual(fn({varA: false, varB: 0, varC: ''}), '');
+      });
+    });
+
+    describe('negation', function() {
+      describe('with variable', function() {
+        it('should parse and evaluate', function() {
+          const fn = compile('!varA');
+          assert.strictEqual(fn({varA: 'x'}), false);
+        });
+      });
+
+      describe('double negation', function() {
+        it('should parse and evaluate', function() {
+          const fn = compile('!!varA');
+          assert.strictEqual(fn({varA: 'x'}), true);
+        });
+      });
+    });
+
+    describe('mixed boolean operators', function() {
+      describe('with disjunction and conjunction 1', function() {
+        it('should parse and evaluate with operator precedence', function() {
+          const fn = compile('!varA || varB && varC');
+          assert.strictEqual(fn({varA: 'x', varB: 42, varC: 0}), 0);
+          assert.strictEqual(fn({varA: 0, varB: 42, varC: false}), true);
+        });
+      });
+    });
+
+    describe('comparison', function() {
+      describe('equality', function() {
+        it('should parse and evaluate', function() {
+          const fn = compile('varA == 42');
+          assert.strictEqual(fn({varA: 42}), true);
+          assert.strictEqual(fn({varA: 23}), false);
+        });
+      });
+
+      describe('inequality', function() {
+        it('should parse and evaluate', function() {
+          const fn = compile('varA != 42');
+          assert.strictEqual(fn({varA: 42}), false);
+          assert.strictEqual(fn({varA: 23}), true);
+        });
+      });
+    });
+
+    describe('comparison and boolean operators', function() {
+      it('should parse and evaluate', function() {
+        const fn = compile('(varA == 42 || varA < 10) && !varB');
+        assert.strictEqual(fn({varA: 42, varB: 'x'}), false);
+        assert.strictEqual(fn({varA: 42, varB: 0}), true);
+        assert.strictEqual(fn({varA: 33, varB: 0}), false);
+        assert.strictEqual(fn({varA: 5, varB: 0}), true);
+      });
+
+      it('should parse and evaluate with operator precedence', function() {
+        const fn = compile('varA == 42 || varA < 10 && !varB');
+        assert.strictEqual(fn({varA: 42, varB: 'x'}), true);
+        assert.strictEqual(fn({varA: 42, varB: 0}), true);
+        assert.strictEqual(fn({varA: 33, varB: 0}), false);
+        assert.strictEqual(fn({varA: 5, varB: 0}), true);
+      });
+    });
+  });
+});
+
