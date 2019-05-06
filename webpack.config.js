d153b28c48c770276a14fe5b706d3311ce8e146d
Christopher Hlubek <hlubek@networkteam.com>
2019-05-06 12:55:36 +0200

    Add webpack for UMD compat

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const config = {
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    library: 'networkteamEel',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[cm]?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
